1c9399452a9cca9d6b11127a45feda504b74a767
Emma Stout <emma.stout01@gmail.com>
2017-10-27 16:18:48 -0500

    add images to page and create gallery controller

--- README.md
+++ README.md
@@ -3,9 +3,9 @@
 ### Tasks 
 - [x] Create basic html, source in angular and js, and get angular working
 - [x] Spin up server
-- [ ] 
-- [ ]
-- [ ]
+- [x] Create gallery controller
+- [x] Add images to page
+- [ ] Create array of image objects
 - [ ]
 - [ ]
 - [ ]
--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,1 +1,21 @@
 var myGalleryApp = angular.module('myGalleryApp', []);
+
+myGalleryApp.controller('GalleryController', function(){
+    var gallery = this;
+    gallery.test = 'true';
+    console.log('controller working');
+    gallery.imageClick = function() {
+        console.log('in image click');
+
+    }
+
+
+
+
+
+
+
+
+
+
+})
